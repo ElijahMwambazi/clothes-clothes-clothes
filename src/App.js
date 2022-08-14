b90584e68e8e12c4ea81b8e03ed33143e30eaa36
Elijah Mwambazi <eijahmwambaxi@gmail.com>
2022-08-15 01:23:04 +0200

    Chore: Added initial categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats"
+    },
+    {
+      id: 2,
+      title: "Jackets"
+    },
+    {
+      id: 3,
+      title: "Sneakers"
+    },
+    {
+      id: 4,
+      title: "Womens"
+    },
+    {
+      id: 5,
+      title: "Mens"
+    },
+  ]
+
   return (
     <div className="App">
-
+      <div className="categories-container">
+        {categories.map(({ id, title }) => (
+          <div key={id} className="category-container">
+            <div className="background-img"></div>
+            <div className="category-content">
+              <h2 className="category-title">{title}</h2>
+              <p className="shop-now">Shop Now</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
