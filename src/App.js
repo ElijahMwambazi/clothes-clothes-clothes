ff530681564dfb8614d5bc9018f4937a46b2931c
Elijah Mwambazi <eijahmwambaxi@gmail.com>
2022-08-17 14:27:05 +0200

    Added navigation, signin, shop componets and logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { Routes, Route } from "react-router-dom"
+import Navigation from "./routes/navigation/navigation.route"
 import Home from "./routes/home/home.route"
+import SignIn from "./routes/sign-in/sign-in.route"
+import Shop from "./routes/shop/shop.route"
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigation />}>
+          <Route path="sign-in" element={<SignIn />} />
+          <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />}></Route>
+        </Route>
       </Routes>
     </div>
   )
