964fb85d7b76f400b26fc5bd04041d9dee96b616
Elijah Mwambazi <eijahmwambaxi@gmail.com>
2022-08-20 04:07:07 +0200

    Implemented sign in with google popup and styled component

--- src/routes/sign-in/sign-in.route.jsx
+++ src/routes/sign-in/sign-in.route.jsx
@@ -2,23 +2,32 @@ import {
   signInWithGooglePopup,
   createUserDocFromAuth,
 } from "../../utils/firebase/firebase.utils";
-import { Fragment } from "react";
 import "./sign-in.styles.scss";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const user = await signInWithGooglePopup();
-    console.log("User data from google: ", user);
-    const userDocRef = await createUserDocFromAuth(user);
+    const { user } = await signInWithGooglePopup();
+
+    try {
+      await createUserDocFromAuth(user);
+    } catch (error) {
+      if (
+        error.code === "auth/popup-closed-by-user" ||
+        error.code === "auth/popup-blocked"
+      )
+        return;
+
+      console.log("Encountered error while creating user:", error.message);
+    }
   };
 
   return (
-    <Fragment>
-      <div className="sign-in">
-        <h1>Sign In</h1>
-        <button onClick={logGoogleUser}>Sign in with Google</button>
-      </div>
-    </Fragment>
+    <div className="sign-in">
+      <h1>Sign In</h1>
+      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <SignUpForm />
+    </div>
   );
 };
 
