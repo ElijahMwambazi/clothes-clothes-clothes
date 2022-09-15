538c58f470d88da9cd25885b17746555a9345d84
Elijah Mwambazi <eijahmwambaxi@gmail.com>
2022-09-15 18:21:57 +0200

    Created shop component ans styled it

--- src/routes/shop/shop.route.jsx
+++ src/routes/shop/shop.route.jsx
@@ -1,3 +1,18 @@
-const Shop = () => {};
+import { useContext, userContext } from "react";
+import { ProductsContext } from "../../contexts/products.context";
+import ProductCard from "../../components/product-card/product-card.components";
+import "./shop.styles.scss";
+
+const Shop = () => {
+  const { products } = useContext(ProductsContext);
+
+  return (
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
 
 export default Shop;
