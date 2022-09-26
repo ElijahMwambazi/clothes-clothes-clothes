0d00f489569e59663ddc6718170f08a078e30a3c
Elijah Mwambazi <eijahmwambaxi@gmail.com>
2022-09-27 00:19:37 +0200

    Added chechout route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import { useContext } from "react"
 import { Routes, Route } from "react-router-dom"
 import Navigation from "./routes/navigation/navigation.route"
 import Home from "./routes/home/home.route"
 import Authentication from "./routes/authentication/authentication.route"
 import Shop from "./routes/shop/shop.route"
+import Checkout from "./routes/checkout/checkout.route"
+import { CartContext } from "./contexts/cart.context"
 
 const App = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+
+  const closeCart = () => {
+    // TODO: check if cart is open
+    if (isCartOpen) {
+      // if open, close cart
+      return setIsCartOpen(!isCartOpen)
+    }
+  }
+
   return (
-    <div className="App">
+    <div className="App" onClick={closeCart}>
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
-          <Route path="shop" element={<Shop />} />
+          <Route path="shop/*" element={<Shop />} />
           <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
         </Route>
       </Routes>
     </div>
