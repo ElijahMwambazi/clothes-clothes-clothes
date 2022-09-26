02e6ef0c6ffd3815ae35a482e0e321c8e3f5eb5a
Elijah Mwambazi <eijahmwambaxi@gmail.com>
2022-09-27 00:18:18 +0200

    Added categories preview in shop and styled it

--- src/routes/shop/shop.route.jsx
+++ src/routes/shop/shop.route.jsx
@@ -1,17 +1,14 @@
-import { useContext, userContext } from "react";
-import { ProductsContext } from "../../contexts/products.context";
-import ProductCard from "../../components/product-card/product-card.components";
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.route";
+import Category from "../category/category.route";
 import "./shop.styles.scss";
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
-
   return (
-    <div className="products-container">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
